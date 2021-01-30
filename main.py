d53035657042e01f0ce793dd3d86ef675b58365d
lo0rd <lo0rd1@bk.ru>
2021-01-30 13:39:42 +0300

    Теперь круг рисуетс в прямоугольнике, как следствие, нет лишних манипуляций с координатами круга. Добавлена платформа, и функции для ее движения. Реализовано взаимодействие шара с платформой.

--- main.py
+++ main.py
@@ -11,21 +11,17 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 platform_width = 400
 platform_height = 50
 platform_left = (screen_width - platform_width) / 2
-platform_buttom = screen_height - platform_height
-platform = pygame.Rect(platform_left, platform_buttom, platform_width, platform_height)
+platform_bottom = screen_height - platform_height
+platform_speed = 20
 
-# player_rect, потом можно удалить
-player_width = 100
-player_height = 100
-player_rect = pygame.Rect(100, 101, 120, 120)  # what is left?
-# left - это координата левой грани нашего прямоугольника
+platform_1 = pygame.Rect(platform_left, platform_bottom, platform_width, platform_height)
 
 # player_circle, добавляем шарик, который потом можно подправить есть метод Rect.center
-center_x = 100
-center_y = 100
-circle_x_speed = 50
-circle_y_speed = 50
-circle_radius = 60
+circle_radius = 30
+circle = pygame.Rect(screen_width / 2, 0, 2 * circle_radius, 2 * circle_radius)
+circle.center = (screen_width / 2, circle_radius)
+circle_x_speed = 5
+circle_y_speed = 6
 
 # FPS
 clock = pygame.time.Clock()
@@ -46,10 +42,18 @@ def game_loop():
                 pygame.quit()
                 quit()
 
+        key = pygame.key.get_pressed()
+        # Движение платформы вправо или влево
+        if key[pygame.K_RIGHT]:
+            moving_platform_right()
+        if key[pygame.K_LEFT]:
+            moving_platform_left()
+
         screen.fill((123, 25, 45))
         moving_circle()
-        #Добавляем платформу, от которой будет отскакивать шарик
-        pygame.draw.rect(screen, (0, 0, 0), platform)
+
+        # Добавляем платформу, от которой будет отскакивать шарик
+        pygame.draw.rect(screen, (0, 0, 0), platform_1)
         pygame.display.update()
         clock.tick(60)
 
@@ -59,23 +63,42 @@ def moving_circle():
     Функция выполняет движение шарика, и осткакивание его от стен.
     :return: Ничего не возвращает
     """
-    global center_x, center_y, circle_y_speed, circle_x_speed
-    center_x += circle_x_speed
-    center_y += circle_y_speed
+    global circle, circle_y_speed, circle_x_speed
+    circle.x += circle_x_speed
+    circle.y += circle_y_speed
 
     # Отскоки от стенок окна для нашего круга
-    if center_x - circle_radius <= 0 or center_x + circle_radius >= screen_width:
+    if circle.left <= 0 or circle.right >= screen_width:
         circle_x_speed *= -1
-    if center_y - circle_radius <= 0 or center_y + circle_radius >= screen_height:
+    if circle.top <= 0:
         circle_y_speed *= -1
+    if circle.bottom >= screen_height:
+        pygame.quit()
 
-    pygame.draw.circle(screen, 'green', (center_x, center_y), circle_radius)
+    # collision ball with platform
+    possible_distance_collision = 5
+    if circle.colliderect(platform_1):
+        if abs(circle.bottom - platform_1.top) <= possible_distance_collision:
+            circle_y_speed *= -1
+
+    pygame.draw.circle(screen, 'green', circle.center, circle_radius)
+    pygame.draw.line(screen, 'black', (screen_width / 2, 0), (screen_width / 2, screen_height))
 
 
 def set_position_from_mouse():
     """Функция устанавливает позицию круга равной позиции мыши"""
     pos = pygame.mouse.get_pos()
     pygame.draw.circle(screen, 'green', pos, circle_radius)
+
+
+def moving_platform_right():
+    """Передвигает платформу вправо"""
+    platform_1.x += platform_speed
+
+
+def moving_platform_left():
+    """Передвигает платформу влево"""
+    platform_1.x -= platform_speed
 
 
 main()
