60d48aa69942b2bbd7b589125affe0dccdb700a5
lo0rd <lo0rd1@bk.ru>
2021-02-01 14:07:01 +0300

    Добавлена вторая платформа. Платформы остаются в пределах игрового окна

--- main.py
+++ main.py
@@ -3,32 +3,29 @@ import pygame
 pygame.init()
 
 # screen
-screen_width = 1500
+screen_width = 700
 screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # planform, платформа - устанавливаем в самом нижу экрана, и по середине
-platform_width = 400
+platform_width = 300
 platform_height = 50
 platform_left = (screen_width - platform_width) / 2
 platform_bottom = screen_height - platform_height
-platform_speed = 20
+platform_speed = 30
 
 platform_1 = pygame.Rect(platform_left, platform_bottom, platform_width, platform_height)
+platform_2 = pygame.Rect(platform_left, 0, platform_width, platform_height)
 
-# player_circle, добавляем шарик, который потом можно подправить есть метод Rect.center
-circle_radius = 30
-circle = pygame.Rect(screen_width / 2, 0, 2 * circle_radius, 2 * circle_radius)
-circle.center = (screen_width / 2, circle_radius)
-circle_x_speed = 5
-circle_y_speed = 6
+# player_circle
+ball_radius = 30
+ball = pygame.Rect(screen_width / 2, 0, 2 * ball_radius, 2 * ball_radius)
+ball.center = (screen_width / 2, screen_height / 2)
+ball_x_speed = 5
+ball_y_speed = 6
 
 # FPS
 clock = pygame.time.Clock()
-
-
-def main():
-    game_loop()
 
 
 # main game loop
@@ -43,16 +40,23 @@ def game_loop():
                 quit()
 
         key = pygame.key.get_pressed()
-        # Движение платформы вправо или влево
+        # Движение нижней платформы вправо или влево
         if key[pygame.K_RIGHT]:
-            moving_platform_right()
+            moving_platform_right(platform_1)
         if key[pygame.K_LEFT]:
-            moving_platform_left()
+            moving_platform_left(platform_1)
+
+        # движение верхней платформы
+        if key[pygame.K_d]:
+            moving_platform_right(platform_2)
+        if key[pygame.K_a]:
+            moving_platform_left(platform_2)
 
         screen.fill((123, 25, 45))
         moving_circle()
 
         # Добавляем платформу, от которой будет отскакивать шарик
+        pygame.draw.rect(screen, (0, 0, 0), platform_2)
         pygame.draw.rect(screen, (0, 0, 0), platform_1)
         pygame.display.update()
         clock.tick(60)
@@ -63,42 +67,42 @@ def moving_circle():
     Функция выполняет движение шарика, и осткакивание его от стен.
     :return: Ничего не возвращает
     """
-    global circle, circle_y_speed, circle_x_speed
-    circle.x += circle_x_speed
-    circle.y += circle_y_speed
+    global ball, ball_y_speed, ball_x_speed
+    ball.x += ball_x_speed
+    ball.y += ball_y_speed
 
     # Отскоки от стенок окна для нашего круга
-    if circle.left <= 0 or circle.right >= screen_width:
-        circle_x_speed *= -1
-    if circle.top <= 0:
-        circle_y_speed *= -1
-    if circle.bottom >= screen_height:
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_x_speed *= -1
+    if ball.bottom >= screen_height or ball.top <= 0:
         pygame.quit()
 
     # collision ball with platform
-    possible_distance_collision = 5
-    if circle.colliderect(platform_1):
-        if abs(circle.bottom - platform_1.top) <= possible_distance_collision:
-            circle_y_speed *= -1
+    possible_distance_collision = 7
+    if ball.colliderect(platform_1) or ball.colliderect(platform_2):
+        if abs(ball.bottom - platform_1.top) <= possible_distance_collision:
+            ball_y_speed *= -1
+        if abs(ball.top - platform_2.bottom) <= possible_distance_collision:
+            ball_y_speed *= -1
 
-    pygame.draw.circle(screen, 'green', circle.center, circle_radius)
-    pygame.draw.line(screen, 'black', (screen_width / 2, 0), (screen_width / 2, screen_height))
-
-
-def set_position_from_mouse():
-    """Функция устанавливает позицию круга равной позиции мыши"""
-    pos = pygame.mouse.get_pos()
-    pygame.draw.circle(screen, 'green', pos, circle_radius)
+    pygame.draw.circle(screen, 'green', ball.center, ball_radius)
+    pygame.draw.line(screen, 'black', (0, screen_height / 2), ( screen_width,  screen_height / 2))
 
 
-def moving_platform_right():
-    """Передвигает платформу вправо"""
-    platform_1.x += platform_speed
+def moving_platform_right(platform):
+    """Передвигает платформу вправо, """
+
+    if platform.right >= screen_width:
+        return
+    else:
+        platform.x += platform_speed
 
 
-def moving_platform_left():
+def moving_platform_left(platform):
     """Передвигает платформу влево"""
-    platform_1.x -= platform_speed
+    if platform.left <= 0:
+        return
+    else:
+        platform.x -= platform_speed
 
-
-main()
+game_loop()
